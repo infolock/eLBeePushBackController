32aa9b39084695e289f5015e40829164f652e90c
Jonathon Hibbard <jhibbard@gmail.com>
2013-07-09 23:51:41 -0400

    Saving

---
+++ eLBeePushBackController/eLBeePushBackController/IntegratedEventsViewController.h
@@ -0,0 +1,13 @@
+//
+//  IntegratedEventsViewController.h
+//  eLBeePushBackController
+//
+//  Created by Jonathon Hibbard on 7/9/13.
+//  Copyright (c) 2013 Integrated Events. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IntegratedEventsViewController : UIViewController
+
+@end
---
+++ eLBeePushBackController/eLBeePushBackController/IntegratedEventsViewController.m
@@ -0,0 +1,29 @@
+//
+//  IntegratedEventsViewController.m
+//  eLBeePushBackController
+//
+//  Created by Jonathon Hibbard on 7/9/13.
+//  Copyright (c) 2013 Integrated Events. All rights reserved.
+//
+
+#import "IntegratedEventsViewController.h"
+
+@interface IntegratedEventsViewController ()
+
+@end
+
+@implementation IntegratedEventsViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
